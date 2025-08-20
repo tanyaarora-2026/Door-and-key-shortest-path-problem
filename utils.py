304351054f8a26debaed9aada707d22c646fa7f9
Tanya Arora <tanyaarora355@gmail.com>
2025-08-20 18:58:47 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,209 @@
+import os
+import numpy as np
+import gymnasium as gym
+import pickle
+import matplotlib.pyplot as plt
+# import imageio
+import imageio.v2 as imageio
+import random
+from minigrid.core.world_object import Goal, Key, Door
+from typing import Iterable, Tuple, Dict, Any, List, Union
+# from create_env import DoorKey10x10Env
+
+MF = 0  # Move Forward
+TL = 1  # Turn Left
+TR = 2  # Turn Right
+PK = 3  # Pickup Key
+UD = 4  # Unlock Door
+
+
+def step_cost(action):
+    # You should implement the stage cost by yourself
+    # Feel free to use it or not
+    # ************************************************
+    return 1  # the cost of action
+
+
+def step(env, action):
+    """
+    Take Action
+    ----------------------------------
+    actions:
+        0 # Move forward (MF)
+        1 # Turn left (TL)
+        2 # Turn right (TR)
+        3 # Pickup the key (PK)
+        4 # Unlock the door (UD)
+    """
+    actions = {
+        0: env.unwrapped.actions.forward,
+        1: env.unwrapped.actions.left,
+        2: env.unwrapped.actions.right,
+        3: env.unwrapped.actions.pickup,
+        4: env.unwrapped.actions.toggle,
+    }
+
+    (obs, reward, terminated, truncated, info) = env.step(actions[action])
+    return step_cost(action), terminated
+
+
+def generate_random_env(seed, task):
+    """
+    Generate a random environment for testing
+    -----------------------------------------
+    seed:
+        A Positive Integer,
+        the same seed always produces the same environment
+    task:
+        'MiniGrid-DoorKey-5x5-v0'
+        'MiniGrid-DoorKey-6x6-v0'
+        'MiniGrid-DoorKey-8x8-v0'
+    """
+    if seed < 0:
+        seed = np.random.randint(50)
+    env = gym.make(task, render_mode="rgb_array")
+    env.reset(seed=seed)
+    return env
+
+
+def load_env(path):
+    """
+    Load Environments
+    ---------------------------------------------
+    Returns:
+        gym-environment, info
+    """
+    with open(path, "rb") as f:
+        env = pickle.load(f)
+
+    info = {
+        "height": env.unwrapped.height, 
+        "width": env.unwrapped.width, 
+        "init_agent_pos": env.unwrapped.agent_pos, 
+        "init_agent_dir": env.unwrapped.dir_vec,
+    }
+
+    for i in range(env.unwrapped.height):
+        for j in range(env.unwrapped.width):
+            if isinstance(env.unwrapped.grid.get(j, i), Key):
+                info["key_pos"] = np.array([j, i])
+            elif isinstance(env.unwrapped.grid.get(j, i), Door):
+                info["door_pos"] = np.array([j, i])
+                # if env.unwrapped.grid.get(j, i).is_open:
+                #     info["door_open"]=(True,)
+                # else:
+                #     info["door_open"]=(False,)
+            elif isinstance(env.unwrapped.grid.get(j, i), Goal):
+                info["goal_pos"] = np.array([j, i])
+                
+
+    return env, info
+
+
+def load_random_env(path):#env_folder):
+    """
+    Load a random DoorKey environment
+    ---------------------------------------------
+    Returns:
+        gym-environment, info
+    """
+    # env_list = [os.path.join(env_folder, env_file) for env_file in os.listdir(env_folder) if env_file.endswith(".env")]
+    # env_path = random.choice(env_list)
+    # with open(env_path, "rb") as f:
+    #     env = pickle.load(f)
+    with open(path, "rb") as f:
+        env = pickle.load(f)
+
+    info = {
+        "height": env.unwrapped.height,
+        "width": env.unwrapped.width,
+        "init_agent_pos": env.unwrapped.agent_pos,
+        "init_agent_dir": env.unwrapped.dir_vec,
+        "door_pos": [],
+        "door_open": [],
+    }
+
+    for i in range(env.unwrapped.height):
+        for j in range(env.unwrapped.width):
+            if isinstance(env.grid.get(j, i), Key):
+                info["key_pos"] = np.array([j, i])
+            elif isinstance(env.grid.get(j, i), Door):
+                info["door_pos"].append(np.array([j, i]))
+                if env.grid.get(j, i).is_open:
+                    info["door_open"].append(True)
+                else:
+                    info["door_open"].append(False)
+            elif isinstance(env.grid.get(j, i), Goal):
+                info["goal_pos"] = np.array([j, i])
+
+    return env, info#, env_path
+
+
+def save_env(env, path):
+    with open(path, "wb") as f:
+        pickle.dump(env, f)
+
+
+def plot_env(env):
+    """
+    Plot current environment
+    ----------------------------------
+    """
+    img = env.render()
+    plt.figure()
+    plt.imshow(img)
+    plt.show()
+
+
+# def draw_gif_from_seq(seq, env, path="./gif/doorkey.gif"):
+#     """
+#     Save gif with a given action sequence
+#     ----------------------------------------
+#     seq:
+#         Action sequence, e.g [0,0,0,0] or [MF, MF, MF, MF]
+
+#     env:
+#         The doorkey environment
+#     """
+#     with imageio.get_writer(path, mode="I", duration=0.8) as writer:
+#         img = env.render()
+#         writer.append_data(img)
+#         for act in seq:
+#             step(env, act)
+#             img = env.render()
+#             writer.append_data(img)
+#     print(f"GIF is written to {path}")
+#     return
+
+  # <- important: use v2 API
+
+def draw_gif_from_seq(seq, env, path="./gif/doorkey.gif", frame_duration=8.0):
+    with imageio.get_writer(path, mode="I", duration=frame_duration, loop=0) as w:
+        img = env.render()
+        w.append_data(img)
+        for act in seq:
+            step(env, act)
+            img = env.render()
+            w.append_data(img)
+    print(f"GIF is written to {path}")
+
+def to_int(x: Any) -> int:
+    """Return a plain Python int from numpy/Python scalar-like values."""
+    return int(x)
+
+def to_bool(x: Any) -> bool:
+    """Return a plain Python bool from numpy/Python bool-like values."""
+    return bool(x)
+
+def to_tuple(xy: Union[Iterable[int], np.ndarray, tuple, list]) -> Tuple[int, int]:
+    a = np.asarray(xy).reshape(2)
+    return (int(a[0]), int(a[1]))
+
+def to_cell_tuple_list(arrs: Iterable[Union[np.ndarray, Iterable[int]]]) -> List[Tuple[int, int]]:
+    return [to_tuple(a) for a in arrs]
+
+def to_cell_tuple_set(arrs: Iterable[Union[np.ndarray, Iterable[int]]]) -> set[Tuple[int, int]]:
+    return set(to_cell_tuple_list(arrs))
+
+
+    
